db8992f9437fd3cfc8687944ea2e18cd72c5e56c
Tzvi Greenfeld <tgreenfeld@tenable.com>
2023-10-29 12:25:55 +0200

    basic traffic light done

--- pages/traffic-light.js
+++ pages/traffic-light.js
@@ -1,8 +1,37 @@
+import { useEffect, useState } from 'react';
+import styles from '../styles/TrafficLight.module.css';
+
+const getNextLight = (currentLight) => {
+    const lights = ['red', 'green', 'yellow'];
+    const currentIndex = lights.indexOf(currentLight);
+    return lights[(currentIndex + 1) % 3];
+};
+
+const Light = ({ color, isOn }) => {
+
+    return <div className={`${styles.light} ${styles[color]} ${!isOn && styles.off}`} />;
+};
 
 export default function TrafficLight() {
+    const [currentLight, setCurrentLight] = useState('red');
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCurrentLight(light => getNextLight(light));
+        }, 1000);
+
+        return () => clearInterval(interval);
+    }, []);
+
+    const isOn = (light) => light === currentLight;
     return (
         <main>
-            wow
-        </main>
+            <div className={styles.lightsContainer}>
+                <Light color="red" isOn={isOn('red')} />
+                <Light color="yellow" isOn={isOn('yellow')} />
+                <Light color="green" isOn={isOn('green')} />
+            </div>
+            <div className={styles.pole} />
+        </main >
     );
 }
